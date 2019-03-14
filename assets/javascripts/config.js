bdd3c1854a87739e73e60efa3f3634926e7269df
hashrock <hashedrock@gmail.com>
2019-03-15 01:58:39 +0900

    chore: add config.js

---
+++ assets/javascripts/config.js
@@ -0,0 +1,21 @@
+//@ts-check
+export const penWidthList = [1, 2, 3, 5, 10, 20, 30, 40, 50];
+export const palette = [
+  "#000000",
+  "#FFFFFF",
+  "#5f5750",
+  "#82759a",
+  "#c0c1c5",
+  "#fff0e7",
+  "#7d2953",
+  "#ff074e",
+  "#ff76a6",
+  "#a95238",
+  "#ffa108",
+  "#feeb2c",
+  "#ffcaa8",
+  "#008551",
+  "#00e339",
+  "#222e53",
+  "#2cabfe"
+];
